ade0f53799c8902cbb5f2bd297f31f9f77656494
LEM-II\Ilja <>
2017-06-10 20:23:53 +0200

    Text Line scripter

---
+++ Assets/Scripts/TextLineScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu()]
+public class LineScript : ScriptableObject {
+    public string line;
+    public AudioClip clip;
+}
