0bb366d4967624d8203a4bdcb3528b7653c0310a
AdronTech <markus.goegele@hotmail.com>
2017-06-11 11:24:00 +0200

    TitleScreen

---
+++ Assets/Scripts/TitleScreen/KeyPressed.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class KeyPressed : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown)
+            SceneManager.LoadScene("Manor");
+	}
+}
---
+++ Assets/Scripts/TitleScreen/REMMoving.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class REMMoving : MonoBehaviour {
+
+	void Update () {
+		
+        transform.localScale = new Vector3(2 + Mathf.Sin(Time.time) * 0.25f, 2 + Mathf.Cos(Time.time) * 0.25f, 1);
+	}
+}
